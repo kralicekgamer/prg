cda44dff551ebf8ba1c2cf91cec8138b9ec24f85
Matěj Adamec <kralicekgamer@gmail.com>
2025-01-20 12:57:02 +0100

    20.01.2025

---
+++ cs/prg/prg/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Zadej číslo, které chceš převést do 2 soustavy: ");
+string i = Console.ReadLine();
+
+int des = int.Parse(i);
---
+++ python/ulohy/10to2.py
@@ -0,0 +1,9 @@
+i = int(input("Zadej číslo které chceš převést do 2 soustavy: "))
+b = []
+res = []
+
+while i > 0:
+    b.insert(0, i % 2)
+    i = i // 2
+
+print("Výsledek je: ", b)
