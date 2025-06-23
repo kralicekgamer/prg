ad2a00baaaa222969d21fedd19f1078f4ed96be8
Matěj Adamec <kralicekgamer@gmail.com>
2025-06-23 20:39:35 +0200

    23.06.2025

---
+++ python/basics/ostatni/datoveTypy.py
@@ -0,0 +1,18 @@
+text = "Hello, world"
+cislo = 5
+des_cislo = 5.5
+
+pravda = True
+nepravda = False
+
+list = ["Hello", "world"]
+tuple = ("Hello", "world")
+
+slovnik = {
+  "znacka": "Ford",
+  "model": "Mustang",
+  "rok": 1964
+}
+
+komplex = complex(1, 2)
+radius = range(5)
